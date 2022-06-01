9bcbb3d59eacfa642326e90716a0cc5939684823
Arsalan <airavani2018@ada.edu.az>
2022-06-01 13:03:49 +0200

    intro

--- src/main.cpp
+++ src/main.cpp
@@ -9,7 +9,6 @@ void setup() {
   lcd.init();
   lcd.clear();
   lcd.backlight();
-
   lcd.createChar(1, Heart);
   lcd.createChar(2, Degree);
   lcd.clear();
@@ -21,15 +20,14 @@ void setup() {
   // LED
   FastLED.addLeds<WS2813, LedPin, RGB>(leds, NUM_LEDS, 0);
   FastLED.setBrightness(LED_MAX_BRIGHTNESS);
-  // FastLED.setMaxPowerInVoltsAndMilliamps(12, 300);
 
   intro();
 
   // Define Colors and Turning LEDS off
-  fill_gradient_RGB(leds, NUM_LEDS, CRGB{0, 255, 0}, CRGB{255, 0, 0});
-
   for (int i = 0; i < NUM_LEDS; i++)
     leds[i].fadeToBlackBy(255);
+
+  fill_gradient_RGB(leds, NUM_LEDS, CRGB{0, 255, 0}, CRGB{255, 0, 0});
 }
 
 void loop() {
@@ -57,9 +55,13 @@ void intro() {
   lcd.write(1);
   delay(1000);
 
-  lcd.setCursor(2, 2);
+  lcd.setCursor(2, 1);
   lcd.print("Mind surrounding");
   delay(1000);
+
+  lcd.setCursor(8, 2);
+  lcd.print("and");
+  delay(500);
 
   lcd.setCursor(2, 3);
   lcd.print("Enjoy your ride!");
@@ -78,10 +80,12 @@ void enableDisplayAndLED(bool turnOn) {
   if (turnOn) {
     lcd.display();
     lcd.backlight();
+
     fill_gradient_RGB(leds, NUM_LEDS, CRGB{0, 255, 0}, CRGB{255, 0, 0});
   } else {
     lcd.noDisplay();
     lcd.noBacklight();
+
     FastLED.clear();
     FastLED.show();
   }
@@ -129,46 +133,6 @@ void ledsLoop() {
     }
 
     FastLED.show();
-  }
-}
-
-void pride() {
-  static uint16_t sPseudotime = 0;
-  static uint16_t sLastMillis = 0;
-  static uint16_t sHue16 = 0;
-
-  uint8_t sat8 = beatsin88(87, 220, 250);
-  uint8_t brightdepth = beatsin88(341, 96, 224);
-  uint16_t brightnessthetainc16 = beatsin88(203, (25 * 256), (40 * 256));
-  uint8_t msmultiplier = beatsin88(147, 23, 60);
-
-  uint16_t hue16 = sHue16; // gHue * 256;
-  uint16_t hueinc16 = beatsin88(113, 1, 3000);
-
-  uint16_t ms = millis();
-  uint16_t deltams = ms - sLastMillis;
-  sLastMillis = ms;
-  sPseudotime += deltams * msmultiplier;
-  sHue16 += deltams * beatsin88(400, 5, 9);
-  uint16_t brightnesstheta16 = sPseudotime;
-
-  for (uint16_t i = 0; i < NUM_LEDS; i++) {
-    hue16 += hueinc16;
-    uint8_t hue8 = hue16 / 256;
-
-    brightnesstheta16 += brightnessthetainc16;
-    uint16_t b16 = sin16(brightnesstheta16) + 32768;
-
-    uint16_t bri16 = (uint32_t)((uint32_t)b16 * (uint32_t)b16) / 65536;
-    uint8_t bri8 = (uint32_t)(((uint32_t)bri16) * brightdepth) / 65536;
-    bri8 += (255 - brightdepth);
-
-    CRGB newcolor = CHSV(hue8, sat8, bri8);
-
-    uint16_t pixelnumber = i;
-    pixelnumber = (NUM_LEDS - 1) - pixelnumber;
-
-    nblend(leds[pixelnumber], newcolor, 64);
   }
 }
 
@@ -304,5 +268,45 @@ void printTemp(String title, int pid, int column, int row) {
     lcd.print(" ");
     lcd.write(2);
     lcd.print("C");
+  }
+}
+
+void pride() {
+  static uint16_t sPseudotime = 0;
+  static uint16_t sLastMillis = 0;
+  static uint16_t sHue16 = 0;
+
+  uint8_t sat8 = beatsin88(87, 220, 250);
+  uint8_t brightdepth = beatsin88(341, 96, 224);
+  uint16_t brightnessthetainc16 = beatsin88(203, (25 * 256), (40 * 256));
+  uint8_t msmultiplier = beatsin88(147, 23, 60);
+
+  uint16_t hue16 = sHue16; // gHue * 256;
+  uint16_t hueinc16 = beatsin88(113, 1, 3000);
+
+  uint16_t ms = millis();
+  uint16_t deltams = ms - sLastMillis;
+  sLastMillis = ms;
+  sPseudotime += deltams * msmultiplier;
+  sHue16 += deltams * beatsin88(400, 5, 9);
+  uint16_t brightnesstheta16 = sPseudotime;
+
+  for (uint16_t i = 0; i < NUM_LEDS; i++) {
+    hue16 += hueinc16;
+    uint8_t hue8 = hue16 / 256;
+
+    brightnesstheta16 += brightnessthetainc16;
+    uint16_t b16 = sin16(brightnesstheta16) + 32768;
+
+    uint16_t bri16 = (uint32_t)((uint32_t)b16 * (uint32_t)b16) / 65536;
+    uint8_t bri8 = (uint32_t)(((uint32_t)bri16) * brightdepth) / 65536;
+    bri8 += (255 - brightdepth);
+
+    CRGB newcolor = CHSV(hue8, sat8, bri8);
+
+    uint16_t pixelnumber = i;
+    pixelnumber = (NUM_LEDS - 1) - pixelnumber;
+
+    nblend(leds[pixelnumber], newcolor, 64);
   }
 }
