b129639a0627f26183605f2ed1181438f555c2f1
Arsalan <airavani2018@ada.edu.az>
2022-06-06 13:27:19 +0200

    sdfgsdfg

--- src/main.cpp
+++ src/main.cpp
@@ -31,7 +31,7 @@ void setup() {
 }
 
 void loop() {
-  delay(10);
+  delay(1);
 
   buttonListener();
 
@@ -99,8 +99,7 @@ void ledsLoop() {
     int level = map(rpm, RPM_MIN, RPM_MAX, 0, NUM_LEDS);
     level = constrain(level, 0, NUM_LEDS);
 
-    fill_gradient_RGB(leds, NUM_LEDS, CRGB{0, 255, 0}, CRGB{255, 255, 0},
-                      CRGB{255, 0, 0});
+    fill_gradient_RGB(leds, NUM_LEDS, CRGB{0, 255, 0}, CRGB{255, 0, 0});
 
     // Turn On
     // for (int i = 0; i < level; i++)
