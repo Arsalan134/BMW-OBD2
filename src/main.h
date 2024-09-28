ee65bd85b64dd36047786b0cc0b08ba51eb0799b
Arsalan Iravani <airavani2018@gmail.com>
2024-09-28 14:43:56 +0200

    add enironment

--- src/main.h
+++ src/main.h
@@ -10,17 +10,32 @@
 // Button
 // Duration in milliseconds to hold to activate long press
 #define LONG_PRESS_TIME 1000
-
 #define DOUBLE_PRESS_TIME_THRESHOLD 1000
 
-// LED
-#define NUM_LEDS 9              // Number of LEDS to use
-#define LED_MAX_BRIGHTNESS 255  // 0 - 255
-#define BLINK_DURATION 75       // milliseconds
+// 🚸🏎️ Comment next line and save to change the Environment
+#define isTesting
+// ----------------------------------------------------
 
-#define RPM_MIN 3000    // Start rpm value for the leds 3000
-#define RPM_MAX 5000    // End rpm value for the leds 5000
-#define BLINK_RPM 5200  // RPM threshold for blink to start 5200
+// LED
+#ifdef isTesting
+// --------------------------- Test Environment ---------------------------------------
+#define NUM_LEDS 9         // Number of LEDS to use
+#define BLINK_DURATION 75  // milliseconds
+
+#define RPM_MIN 700               // Start rpm value
+#define RPM_MAX 2000              // End rpm value
+#define BLINK_RPM_THRESHOLD 2300  // RPM threshold for blink to start
+
+#else
+// --------------------------- Real Environment ---------------------------------------
+#define NUM_LEDS 9         // Number of LEDS to use
+#define BLINK_DURATION 75  // milliseconds
+
+#define RPM_MIN 3000              // Start rpm value
+#define RPM_MAX 5000              // End rpm value
+#define BLINK_RPM_THRESHOLD 5200  // RPM threshold for blink to start
+
+#endif
 
 // Threshold at which arduino considers engine turned off
 #define TURN_OFF_RPM 100
